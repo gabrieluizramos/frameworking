167b060100e5392a45c9f8cd468f48d600e32882
KBR 11 <suporte@kbrtec.com.br>
2016-03-11 16:58:32 -0300

    initializing gh-pages

---
+++ js/plugins.extended.js
@@ -0,0 +1,67 @@
+$.extend({
+	init: function( exec, event, trigger, target, time ){
+		//<--
+		//it's possible to execute using 'eval' js method
+		//exec = "$."+exec;
+		//eval( exec )( event, trigger, target );
+		//but let's use only jquery 
+		//-->
+		final = true;
+		console.log( "<-----" );
+		try{
+			console.debug( "Init: "+ exec );
+			if ( exec && typeof exec == 'string' ) {
+				$[ exec ]( event , trigger , target , time );
+			}
+		} 
+		catch( err ){
+			console.trace( "Error trying to execute "+ exec );
+			console.trace("Error: "+ err);
+			final = false;
+		}
+		finally{
+			console.debug( "Execution: "+ exec +" complete [ "+ final +" ]" );
+		}
+		console.log( "----->\n\n" );
+	},
+	dropdown: function( event, trigger, target, time ){
+		$( trigger )
+		.unbind( event )
+		.bind( event, function(){
+			if ( $( trigger ).hasClass( "active" ) ) {
+				$( trigger )
+				.toggleClass( "active" )
+				.next( target )
+				.slideToggle( time )
+				.toggleClass( "active" );
+				return;
+			}
+			$( trigger )
+			.toggleClass( "active" )
+			.next( target )
+			.slideToggle( time )
+			.toggleClass( "active" );
+		});
+	},
+	slider: function( event, trigger, target, time ){
+		$( trigger )
+		.unbind( event )
+		.bind( event, function(){
+
+			$( target )
+			.removeClass( "active" )
+			.slideUp( time );
+
+			$( trigger )
+			.removeClass( 'active' );
+
+			$( target+':nth-child('+ ( $(this).index()+1) +')' )
+			.slideDown( time )
+			.addClass( 'active' );
+
+			$( this )
+			.addClass( 'active' );
+		});
+		$( trigger+':nth-child(1) ' ).trigger("click");
+	}
+});
---
+++ js/plugins.js
@@ -0,0 +1,78 @@
+<!-- 
+GABRIEL RAMOS FRAMEWORK
+PLUGINS BASED ONLY JQUERY LIB
+-->
+<!-- PLUGINS -->
+<!-- JQUERY -->
+<script src="jquery-1.11.3.min.js"></script>
+
+
+<!-- ACCORDION ADDING/REMOVING CLASS ACTIVE ON PARENT ELEMENT -->
+<script>
+    $('.accordion .accordion-link').on('click',function(){
+        // CHECK, IF THE ELEMENT CLICKED IS ACTIVE, IT CLOSES AND BREAK THE FUNCTION
+        // COMMENT THIS 'IF' IF YOU WANT AN 'UNACTIVE' ACCORDION
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active').next('.accordion-content').slideToggle();
+            return;
+        }
+        $('.accordion-link.active').removeClass('active').next('.accordion-content').slideToggle();
+        $(this).addClass('active').next('.accordion-content').slideToggle();
+    });
+// INITIALIZE FIRST ACCORDION ITEM ACTIVE
+$(document).ready(function(){
+    $('.accordion-item:first-child .accordion-link').click();
+});
+</script>
+
+
+<!-- SLIDER UP AND DOWN ADDING/REMOVING CLASS ACTIVE ON MENU LINKS -->
+<script>
+    $('.slider-menu-link').on('click',function(){
+        $('.slider-menu-link').removeClass('active');
+        $('.slider-image').removeClass('active').slideUp().fadeOut(200);
+        $('.slider-image:nth-child('+($(this).index()+1)+')').slideDown().fadeIn(600).addClass('active');
+        $(this).addClass('active');
+        clearInterval(sliderAuto);
+        sliderAuto = setInterval(slider, 5000);
+    });
+    // AUTOMATIC PLAY, 5 SECONDS
+    function slider(){
+        var nextActiveItem = $('.slider-menu-link.active').index()+2;
+        var totalItem = $('.slider-menu-link').length;
+        if (nextActiveItem > totalItem) {
+            nextActiveItem = 1;
+        }
+        $('.slider-menu-link:nth-child('+nextActiveItem+')').click();
+    }
+    function sliderPrev(){
+        var activeItem = $('.slider-menu-link.active').index();
+        var nextActiveItem = activeItem;
+        if (nextActiveItem < 1) {
+            nextActiveItem = totalItem;
+        }
+        $('.slider-menu-link:nth-child('+nextActiveItem+')').click();
+    }
+// INITIALIZE FIRST SLIDER MENU LINK CLICKED
+$(document).ready(function(){
+    $('.slider-menu-link:first-child').click();
+});
+// SET INTERVAL FOR ALL SLIDERS
+$(document).ready(function(){
+    var sliderAuto = setInterval(slider, 5000);
+});
+$('.slider-next').click(function(){
+    slider();
+});
+$('.slider-prev').click(function(){
+    sliderPrev();
+});
+</script>
+
+<!-- DONE -->
+<!-- DROP DOWN (LIKE RESPONSIVE MENUS) -->
+<script>
+    $('.drop-link').on('click',function(){
+        $(this).toggleClass('active').next('.drop-content').slideToggle();
+    });
+</script>
